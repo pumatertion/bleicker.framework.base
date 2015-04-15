63ce759c6fe96d73448bc9f12d7dc2fb8bc974f6
Carsten Bleicker <carsten@bleicker.de>
2015-04-15 21:14:49 +0200

    StandardController passing http query argument to view

--- src/Foo/Controller/StandardController.php
+++ src/Foo/Controller/StandardController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Foo\Controller;
+use Bleicker\Request\Http\Request;
 
 use Bleicker\Controller\AbstractController;
 
@@ -15,6 +16,9 @@ class StandardController extends AbstractController {
 	 * @return void
 	 */
 	public function indexAction() {
-		$this->view->assign('title', 'Hello World');
+		/** @var Request $httpRequest */
+		$httpRequest = $this->request->getMainRequest();
+		$title = $httpRequest->query->has('title') ? $httpRequest->query->get('title') : 'Hello World';
+		$this->view->assign('title', $title);
 	}
 }
