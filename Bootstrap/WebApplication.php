de39037cfdde5a84d8c84d9189fb022cdfbaa60d
Carsten Bleicker <carsten@bleicker.de>
2015-04-20 12:53:30 +0200

    Use static registry

--- Bootstrap/WebApplication.php
+++ Bootstrap/WebApplication.php
@@ -4,14 +4,14 @@ define('ROOT_DIRECTORY', __DIR__ . '/..');
 
 use App\Controller\HomeController;
 use App\Controller\StandardController;
+use Bleicker\Framework\Registry;
 use Bleicker\Framework\WebApplication;
 use Bleicker\Routing\ControllerRouteData;
 use Bleicker\Routing\RouterInterface;
 
 include __DIR__ . "/../vendor/autoload.php";
 
-
-$app = WebApplication::getInstance();
+$app = new WebApplication();
 
 /**
  * Generating Application Registry
@@ -19,13 +19,12 @@ $app = WebApplication::getInstance();
  * @example $app->getRegistry()->addImplementation(Whatever::class, function () {});
  */
 
-
 /**
  * Routing Configuration
  *
  * @var RouterInterface $router
  */
-$router = WebApplication::getRegistry()->getImplementation(RouterInterface::class);
+$router = Registry::getImplementation(RouterInterface::class);
 $router->addRoute('/', 'get', new ControllerRouteData(HomeController::class, 'indexAction'));
 $router->addRoute('/user/{user}', 'get', new ControllerRouteData(StandardController::class, 'indexAction'));
 
