e7688637bcaf9f1e7fbf913f9a3c035b9fdb117e
Carsten Bleicker <carsten@bleicker.de>
2015-04-21 14:18:53 +0200

    Cleanup Routing Configuration

--- Configuration/Routing.php
+++ Configuration/Routing.php
@@ -5,11 +5,6 @@ use Bleicker\Framework\Registry;
 use Bleicker\Routing\ControllerRouteData;
 use Bleicker\Routing\RouterInterface;
 
-/**
- * Routing Configuration
- *
- * @var RouterInterface $router
- */
-$router = Registry::getImplementation(RouterInterface::class);
-$router->addRoute('/', 'get', new ControllerRouteData(ExampleController::class, 'indexAction'));
-$router->addRoute('/user/{userName}', 'get', new ControllerRouteData(ExampleController::class, 'userAction'));
+Registry::getImplementation(RouterInterface::class)
+	->addRoute('/', 'get', new ControllerRouteData(ExampleController::class, 'indexAction'))
+	->addRoute('/user/{userName}', 'get', new ControllerRouteData(ExampleController::class, 'userAction'));
