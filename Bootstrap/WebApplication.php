d2b0d07d3c4b8da457e8b112205086191abd2a58
Carsten Bleicker <carsten@bleicker.de>
2015-04-21 13:12:42 +0200

    Introduce ContextInterface configuration

--- Bootstrap/WebApplication.php
+++ Bootstrap/WebApplication.php
@@ -4,9 +4,11 @@ use Bleicker\Framework\WebApplication;
 
 include __DIR__ . '/../vendor/autoload.php';
 include __DIR__ . '/../Configuration/Constants.php';
+include __DIR__ . '/../Configuration/Context.php';
 
 $app = new WebApplication();
 
 include __DIR__ . '/../Configuration/Routing.php';
 include __DIR__ . '/../Configuration/Cache.php';
+include __DIR__ . '/../Configuration/Persistence.php';
 
--- Configuration/Cache.php
+++ Configuration/Cache.php
@@ -1,12 +1,14 @@
 <?php
 
+use Bleicker\Framework\Context\Production;
 use Bleicker\Framework\Registry;
 use TYPO3\Fluid\Core\Cache\FluidCacheInterface;
 use TYPO3\Fluid\Core\Cache\SimpleFileCache;
+use Bleicker\Framework\Context\ContextInterface;
 
 /**
  * Cache Configurations
  */
-if (Registry::get('CONTEXT') === 'production') {
+if (Registry::getImplementation(ContextInterface::class) instanceof Production) {
 	Registry::addImplementation(FluidCacheInterface::class, new SimpleFileCache(ROOT_DIRECTORY . '/Cache'));
 }
