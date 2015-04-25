4149de170ef2f3562701c8fe3f05866f3873cdf4
Carsten Bleicker <carsten@bleicker.de>
2015-04-25 21:39:49 +0200

    Use MockArraySessionStorage for FunctionalTests

--- tests/FunctionalTestCase.php
+++ tests/FunctionalTestCase.php
@@ -2,6 +2,13 @@
 namespace Tests\App;
 
 use Bleicker\Framework\ApplicationInterface;
+use Bleicker\ObjectManager\ObjectManager;
+use Bleicker\Request\MainRequestInterface;
+use Bleicker\Session\Session;
+use Bleicker\Session\SessionInterface;
+use Symfony\Component\HttpFoundation\Session\Storage\MockArraySessionStorage;
+use Bleicker\Request\RequestInterface;
+use Bleicker\Framework\Http\Request;
 
 /**
  * Class UnitTestCase
@@ -18,5 +25,9 @@ abstract class FunctionalTestCase extends BaseTestCase {
 	protected function setUp() {
 		parent::setUp();
 		include_once __DIR__ . '/../Bootstrap/Webapplication.php';
+		ObjectManager::register(SessionInterface::class, new Session(new MockArraySessionStorage()));
+		/** @var Request $request */
+		$request = ObjectManager::get(MainRequestInterface::class);
+		$request->setSession(ObjectManager::get(SessionInterface::class));
 	}
 }
